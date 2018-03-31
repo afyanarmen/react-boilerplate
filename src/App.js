d600b1a9c2c5fb1ede4dc3d6714499352edca56e
tech-lab <tech-lab@itc-lab>
2018-03-31 12:25:03 +0400

    thuis

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+    value: ""
+  }};
+  click =(e)=>{
+
+    this.setState({
+       value:   `${this.state.value}${e.target.value}`
+    });
+
+  }
+
   render() {
+
     return (
       <div className="App">
-        <p>Hello React World</p>
+        <input type = "text" value ={this.state.value}></input>
+      <button value ={1} onClick ={this.click}>1</button>
+      <button value ={2} onClick ={this.click}>2</button>
+      <button value ={3} onClick ={this.click}>3</button>
       </div>
     );
-  }
+
+}
 }
 
 export default App;
