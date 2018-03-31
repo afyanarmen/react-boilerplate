f7a84f9ca1428462985bf6531add061d21ee0409
tech-lab <tech-lab@itc-lab>
2018-03-31 13:31:53 +0400

    thuis

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
   constructor(){
     super();
@@ -7,21 +8,37 @@ class App extends Component {
     value: ""
   }};
   click =(e)=>{
-
-    this.setState({
+this.setState((state) => ({ value: state.value + 1}));
+this.setState({
        value:   `${this.state.value}${e.target.value}`
+
     });
-
   }
-
+  vclick =(e)=>{
+    this.setState({value: eval(this.state.value)});
+  }
   render() {
 
     return (
       <div className="App">
-        <input type = "text" value ={this.state.value}></input>
+        <input type = "text" value ={this.state.value} disabled></input>
+        <p>
       <button value ={1} onClick ={this.click}>1</button>
-      <button value ={2} onClick ={this.click}>2</button>
       <button value ={3} onClick ={this.click}>3</button>
+      <button value ={4} onClick ={this.click}>4</button>
+      </p>
+      <button value ={5} onClick ={this.click}>5</button>
+      <button value ={6} onClick ={this.click}>6</button>
+      <button value ={7} onClick ={this.click}>7</button>
+      <p>
+      <button value ={8} onClick ={this.click}>8</button>
+      <button value ={9} onClick ={this.click}>9</button>
+      <button value ={0} onClick ={this.click}>0</button>
+      </p>
+
+      <button value ="+" onClick ={this.click}>+</button>
+      <button value ="=" onClick ={this.vclick}>=</button>
+
       </div>
     );
 
